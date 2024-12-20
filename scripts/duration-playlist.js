aecd67c2b6b12a93ce73170fd64618a5755c1970
stlee42 <42285007+stlee42@users.noreply.github.com>
2024-12-20 17:55:13 +0900

    fix dark mode bug
    Fix the bug that the extension thinks it's in dark mode even when it's
    supposed to be in light mode.

--- scripts/duration-playlist.js
+++ scripts/duration-playlist.js
@@ -53,7 +53,12 @@ const appendUiElement = () => {
 }
 
 const checkTheme = () => {
-    return (document.querySelector("[dark]")) ? ("dark") : ("light");
+    if ( window &&
+         window.matchMedia &&
+         window.matchMedia('(prefers-color-scheme: dark)').matches ) {
+        return ("dark");
+    }
+    return ("light");
 }
 
 const getVideoTimeList = () => {
